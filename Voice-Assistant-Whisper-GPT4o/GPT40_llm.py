63039b22774a2c07dee6607051cae92505687a68
Mohamed Fahim <143820658+AAMohamedFahim@users.noreply.github.com>
2024-07-08 06:30:13 +0530

    Update GPT40_llm.py

--- Voice-Assistant-Whisper-GPT4o/GPT40_llm.py
+++ Voice-Assistant-Whisper-GPT4o/GPT40_llm.py
@@ -1,15 +1,15 @@
 from openai import AzureOpenAI
 import os
-import dotenv
+# import dotenv
 
 def LLM_response(prompt):
-    dotenv.load_dotenv()
+    # dotenv.load_dotenv()
     client = AzureOpenAI(
-      azure_endpoint = os.environ["Azure_Endpoints"], 
-      api_key=os.environ['Azure_Openai_Api'],  
+      azure_endpoint = st.secrets["Azure_Endpoints"], 
+      api_key=st.secrets['Azure_Openai_Api'],  
       api_version = "2024-02-01"
       )
-    deployment=os.environ['Azure_Deployements']
+    deployment=st.secrets['Azure_Deployements']
 
     # prompt = input("user : ")
     messages = [{"role": "user", "content": "you are the helpfull assistance to assist user, dont use punctuation in output"},
